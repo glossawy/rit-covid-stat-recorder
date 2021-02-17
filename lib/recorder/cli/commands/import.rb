4c8a93b98a5397a3a3a0a04bf9b263c215021af1
Glossawy <4606234+Glossawy@users.noreply.github.com>
2021-02-17 17:25:39 -0500

    rename to match convention

--- lib/recorder/cli/commands/import.rb
+++ lib/recorder/cli/commands/import.rb
@@ -2,12 +2,12 @@ module Recorder
   class CLI
     module Commands
       module Import
-        require 'recorder/cli/commands/import/rit-covid-dashboard-api'
+        require 'recorder/cli/commands/import/rit_covid_dashboard_api'
       end
     end
 
     register 'import', aliases: ['i'] do |prefix|
-      prefix.register 'rit-covid-dashboard-api', Commands::Import::RitCovidDashboardApi, aliases: ['api', 'rcd-api']
+      prefix.register 'rit-covid-dashboard-api', Commands::Import::RitCovidDashboardApi, aliases: ['rcd-api']
     end
   end
 end
